3c7afa2e4ea5752f98deab30afe334ca3c317ec2
Capti <captianbeau@gmail.com>
2023-11-02 16:27:43 -0600

    reorganized and added code

--- script.js
+++ script.js
@@ -1,52 +1,32 @@
 // need 8-128 char, lowercase, uppercase, numeric, and/or special characters
 // Assignment Code
+// the #generate refers to the button
+var generateBtn = document.querySelector("#generate");
 
-  var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
-  var gen = true;
+  function generatePassword(){
+    var char = window.prompt("How many characters between 8-128?");
+    var num = window.confirm("Do you want numbers?");
+    var charS = window.confirm("Do you want special characters?")
+    var charU = window.confirm("Do you want uppercase letters")
+    var charL = window.confirm("Do you want lowercase letters?")
 
-  var specialCharacters = ['@','$', '*', '!', '%', '&', '#', '?', '>', '.', '=', '+', '(', '-', ')', '<', '/', '\\', ';', '[', ']', ':', '{', '}', '|', '“', '“”', '_'];
-  var numbers = ['1','2','3','4','5','6','7','8','9'];
-  var upletters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  var lowletters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  
-  while (gen == true){
-  var char = window.prompt("How many characters between 8-128?");
-  var num = window.alert("Do you want numbers?");
-  var charS = window.alert("Do you want special characters?")
-  var charU = window.alert("Do you want uppercase letters")
-  var charL = window.alert("Do you want lowercase letters?")
-  var password = generatePassword(){
-    for ( i = 0; i <char;i++){
+    var specialCharacters = ['@','$', '*', '!', '%', '&', '#', '?', '>', '.', '=', '+', '(', '-', ')', '<', '/', '\\', ';', '[', ']', ':', '{', '}', '|', '“', '“”', '_'];
+    var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+    var upletters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    var lowletters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-    }
+    if
 
   }
-
+  // the #pass word refers to the text area
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  var password = generatePassword();
 
-}
+
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// prompt("Enter Password")
-// if(prompt == true){
-// return Math.random(specialCharacters)
-//}
-//  for (var i = 8; i<= 128; i++){}
-
-// function makeid(length) {
-//   let result = '';
-//   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//   const charactersLength = characters.length;
-//   let counter = 0;
-//   while (counter < length) {
-//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//     counter += 1;
-//   }
-//   return result;
-// }
--- test.js
+++ test.js
@@ -21,3 +21,40 @@ for( i = 0; i < char; i++){
 var password = requirements[Math.floor(Math.random() * requirements.length)];
     }
     console.log(password)
+    // prompt("Enter Password")
+// if(prompt == true){
+// return Math.random(specialCharacters)
+//}
+//  for (var i = 8; i<= 128; i++){}
+
+// function makeid(length) {
+//   let result = '';
+//   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//   const charactersLength = characters.length;
+//   let counter = 0;
+//   while (counter < length) {
+//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//     counter += 1;
+//   }
+//   return result;
+// }
+var gen = true;
+
+var specialCharacters = ['@','$', '*', '!', '%', '&', '#', '?', '>', '.', '=', '+', '(', '-', ')', '<', '/', '\\', ';', '[', ']', ':', '{', '}', '|', '“', '“”', '_'];
+var numbers = ['1','2','3','4','5','6','7','8','9'];
+var upletters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var lowletters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+while (gen == true){
+var char = window.prompt("How many characters between 8-128?");
+var num = window.alert("Do you want numbers?");
+var charS = window.alert("Do you want special characters?")
+var charU = window.alert("Do you want uppercase letters")
+var charL = window.alert("Do you want lowercase letters?")
+{
+  for ( i = 0; i <char;i++){
+
+  }
+
+}
+}
