eb090fdc19679a3eef3912c64b13677542b825b2
Capti <captianbeau@gmail.com>
2023-11-03 13:55:59 -0600

    organized and added code

--- script.js
+++ script.js
@@ -2,76 +2,55 @@
 // Assignment Code
 // the #generate refers to the button
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var char = window.prompt("How many characters between 8-128?");
+  // figure out how to isolate char so you don't have to go through every thing
+  if (8 > char || char > 128) {
+    return "Must be between 8 and 128"
+  }
+  var password = ""
+  var num = window.confirm("Do you want numbers?");
+  var charS = window.confirm("Do you want special characters?");
+  var charU = window.confirm("Do you want uppercase letters");
+  var charL = window.confirm("Do you want lowercase letters?");
 
-// Write password to the #password input
-function writePassword() {
-  function generatePassword(){
-    var char = window.prompt("How many characters between 8-128?");
-    // figure out how to isolate char so you don't have to go through every thing
-    var num = window.confirm("Do you want numbers?");
-    var charS = window.confirm("Do you want special characters?")
-    var charU = window.confirm("Do you want uppercase letters")
-    var charL = window.confirm("Do you want lowercase letters?")
-    
-    // var num = false
-    // var charS = false
-    // var charU = false
-    // var charL = false
 
-    var specialCharacters = ['@','$', '*', '!', '%', '&', '#', '?', '>', '.', '=', '+', '(', '-', ')', '<', '/', '\\', ';', '[', ']', ':', '{', '}', '|', '“', '“”', '_'];
-    var numbers = ['0','1','2','3','4','5','6','7','8','9'];
-    var upLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    var lowLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    
-    if(num == true || charS == true || charU == true || charL == true){
-      // add all possibilities into the switch
-    switch(true){
-      case num == charS == charU == charL:
-        var requirements = numbers.concat(specialCharacters,upLetters,lowLetters)
-        for( i = 8 ; i <= char; i++ ){
-        var index = Math.floor(Math.random() * requirements.length);
-        password = password + requirements[index]
-        }
-      break;
-      case num == charS == charU:
-        var requirements = numbers.concat(specialCharacters,upLetters)
-        for( i = 8 ; i <= char; i++ ){
-        var index = Math.floor(Math.random() * requirements.length);
-        password = password + requirements[index]
-        }
-      break;
-      case charS:
-          
-      break;
-      case charU:
-          
-      break;
-      case charL:
-          
-      break;
-      case num:
-          
-      break;
-      default:
-        
-          
+  var specialCharacters = ['@', '$', '*', '!', '%', '&', '#', '?', '>', '.', '=', '+', '(', '-', ')', '<', '/', '\\', ';', '[', ']', ':', '{', '}', '|', '“', '“”', '_'];
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var upLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  var lowLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  var requirements = []
 
-  
+  if (num == true || charS == true || charU == true || charL == true) {
+    if (num) {
+      requirements = requirements.concat(numbers)
     }
-  }  else if( 8 > char || char > 128 && num != true || charS != true || charU != true || charL != true){
-    password = "Must be between 8 and 128 and Must choose at least one character type"
-  } else if ( 8 > char || char > 128){
-    password = "Must be between 8 and 128"
-  } else{
-    password = "Must choose at least one character type"
+    if (charS) {
+      requirements = requirements.concat(specialCharacters)
+    }
+    if (charU) {
+      requirements = requirements.concat(upLetters)
+    }
+    if (charL) {
+      requirements = requirements.concat(lowLetters)
+    } 
+  for (i = 8; i <= char; i++) {
+    var index = Math.floor(Math.random() * requirements.length);
+    password = password + requirements[index]
   }
   return password
-  }
-  
+} else {
+  return "Must choose at least one character type"
+}
+}
+// Write password to the #password input
+function writePassword() {
+
+
   // the #pass word refers to the text area
   var passwordText = document.querySelector("#password");
-  var password = "";
-  password = generatePassword();
+  
+  var password = generatePassword();
 
   passwordText.value = password;
 }
