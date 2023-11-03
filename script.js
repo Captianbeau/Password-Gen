c50abc3e42b4f89ceacc3a64b123870daf0d93cb
Capti <captianbeau@gmail.com>
2023-11-03 16:25:08 -0600

    cleaned and comments

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-// need 8-128 char, lowercase, uppercase, numeric, and/or special characters
-// Assignment Code
 // the #generate refers to the button
 var generateBtn = document.querySelector("#generate");
+
+
 function generatePassword() {
+
+// pop ups start
   var char = window.prompt("How many characters between 8-128?");
-  // figure out how to isolate char so you don't have to go through every thing
+
+// stops the code if user inputs wrong numbers
   if (8 > char || char > 128) {
     return "Must be between 8 and 128"
   }
-  var password = ""
+  
   var num = window.confirm("Do you want numbers?");
   var charS = window.confirm("Do you want special characters?");
   var charU = window.confirm("Do you want uppercase letters");
   var charL = window.confirm("Do you want lowercase letters?");
+// end pop ups
 
-
+// arrays start
+  var password = ""
   var specialCharacters = ['@', '$', '*', '!', '%', '&', '#', '?', '>', '.', '=', '+', '(', '-', ')', '<', '/', '\\', ';', '[', ']', ':', '{', '}', '|', '“', '“”', '_'];
   var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var upLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   var lowLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   var requirements = []
+//arrays end 
 
+// if statements start
   if (num == true || charS == true || charU == true || charL == true) {
     if (num) {
       requirements = requirements.concat(numbers)
@@ -33,27 +40,28 @@ function generatePassword() {
     }
     if (charL) {
       requirements = requirements.concat(lowLetters)
-    } 
-  for (i = 8; i <= char; i++) {
-    var index = Math.floor(Math.random() * requirements.length);
-    password = password + requirements[index]
-  }
+    }
+    for (i = 8; i <= char; i++) {
+      var index = Math.floor(Math.random() * requirements.length);
+      password = password + requirements[index]
+    }
+    } else {
+      return "Must choose at least one character type"
+    }
+// end if statements
   return password
-} else {
-  return "Must choose at least one character type"
 }
-}
-// Write password to the #password input
+// end function generatePassword
+
 function writePassword() {
 
-
-  // the #pass word refers to the text area
+// the #pass word refers to the text area
   var passwordText = document.querySelector("#password");
   
   var password = generatePassword();
 
   passwordText.value = password;
 }
-
-// Add event listener to generate button
+// end function writePassword
+//event listener to generate button
 generateBtn.addEventListener("click", writePassword);
